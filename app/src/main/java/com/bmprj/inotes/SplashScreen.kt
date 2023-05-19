6b2ab566ad72b6cba45c31c218847b904a31eac2
Busra <busramacak@outlook.com>
2023-05-19 17:11:08 +0300

    app icon append

--- app/src/main/java/com/bmprj/inotes/SplashScreen.kt
+++ app/src/main/java/com/bmprj/inotes/SplashScreen.kt
@@ -1,18 +1,24 @@
 package com.bmprj.inotes
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
 
+@Suppress("DEPRECATION")
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     val cs = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            FLAG_FULLSCREEN,
+            FLAG_FULLSCREEN
+        )
 
         cs.launch {
             delay(3000)
